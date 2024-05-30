934e5fe888fe35cc09bb588148bd5b0bdca9d348
Siddhesh Jondhale <84581657+Siddheshjondhale@users.noreply.github.com>
2024-05-31 01:15:40 +0530

    Changes

--- src/context/AccountProvider.jsx
+++ src/context/AccountProvider.jsx
@@ -15,7 +15,10 @@
         const [newMessageFlag, setNewMessageFlag]=useState(false)
 
         useEffect(()=>{
+
             socket.current = io('https://messagingappsocket.onrender.com');
+            // socket.current=io('ws://localhost:9000')
+
         }, [])
         return(
 
