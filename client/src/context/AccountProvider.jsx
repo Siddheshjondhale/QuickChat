4b4ef2132df775768465f93fcbbba07e646a6379
Siddhesh Jondhale <84581657+Siddheshjondhale@users.noreply.github.com>
2024-05-31 20:12:37 +0530

    changeds

--- client/src/context/AccountProvider.jsx
+++ client/src/context/AccountProvider.jsx
@@ -1,46 +1,36 @@
-    import { createContext, useEffect, useRef, useState } from "react"
+import React, { createContext, useEffect, useRef, useState } from 'react';
+import { io } from 'socket.io-client';
 
-    import {io} from 'socket.io-client'
+export const AccountContext = createContext(null);
 
-    export const AccountContext=createContext(null);
+const AccountProvider = ({ children }) => {
+    const [account, setAccount] = useState(null);
+    const [person, setPerson] = useState({});
+    const [active, setActive] = useState([]);
+    const socket = useRef();
+    const [newMessageFlag, setNewMessageFlag] = useState(false);
 
+    useEffect(() => {
+        socket.current = io('https://messagingappsocket.onrender.com');
+    }, []);
 
-    const AccountProvider =({children})=>{
-
-
-        const[account,setAccount] = useState(null);
-        const[person,setPerson] = useState({});
-        const[active,setActive]=useState([])
-        const socket=useRef()
-        const [newMessageFlag, setNewMessageFlag]=useState(false)
-
-        useEffect(()=>{
-
-            socket.current = io('https://messagingappsocket.onrender.com');
-           
-
-        }, [])
-        return(
-
-            <AccountContext.Provider value={
-                
-                {account,setAccount,
-                person, setPerson,socket,active,setActive,newMessageFlag,setNewMessageFlag
-                }
-               
-            
-            }
-                
-                
-                >
-
+    return (
+        <AccountContext.Provider
+            value={{
+                account,
+                setAccount,
+                person,
+                setPerson,
+                socket,
+                active,
+                setActive,
+                newMessageFlag,
+                setNewMessageFlag
+            }}
+        >
             {children}
+        </AccountContext.Provider>
+    );
+};
 
-            </AccountContext.Provider>
-
-
-    
-        )
-
-    }
-    export default AccountProvider;
+export default AccountProvider;
