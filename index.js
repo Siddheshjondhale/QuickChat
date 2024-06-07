674cc4ab08e68816fa4bbcb7f69945213a25f94e
Siddhesh Jondhale <84581657+Siddheshjondhale@users.noreply.github.com>
2024-06-08 02:18:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ import cors from 'cors'
 import bodyParser from 'body-parser';
 const app = express();
 
+// Configure CORS to allow requests from your client domain
+const corsOptions = {
+  origin: 'https://messaging-app-latest.vercel.app', // Update this to match your client's URL
+  optionsSuccessStatus: 200
+};
 
-app.use(cors())
+app.use(cors(corsOptions));
 app.use(bodyParser.json({extended:true}))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/',route)
